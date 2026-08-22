cb8565d7e7f4ee84657580b0f1af27cf4ec3bf3b
AKASHA HUB <yanfili.simon@gmail.com>
2026-08-22 01:18:50 -0300

    Update firebase-config.js

--- afplataforma/firebase/firebase-config.js
+++ afplataforma/firebase/firebase-config.js
@@ -1,23 +1,20 @@
 <script type="module">
   // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-analytics.js";
   // TODO: Add SDKs for Firebase products that you want to use
   // https://firebase.google.com/docs/web/setup#available-libraries
 
   // Your web app's Firebase configuration
-  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   const firebaseConfig = {
-    apiKey: "AIzaSyC7Vb3v2KKnMTlaRENGLpZOqrzqFAD1zc4",
-    authDomain: "akashahubaf.firebaseapp.com",
-    projectId: "akashahubaf",
-    storageBucket: "akashahubaf.firebasestorage.app",
-    messagingSenderId: "825764077352",
-    appId: "1:825764077352:web:406555563a380829b2fa1d",
-    measurementId: "G-LWBLGM2545"
+    apiKey: "AIzaSyAQXJDGfsd7RgcYKm9wfuh6nOth7dWo-v4",
+    authDomain: "hub-akasha.firebaseapp.com",
+    databaseURL: "https://hub-akasha-default-rtdb.firebaseio.com",
+    projectId: "hub-akasha",
+    storageBucket: "hub-akasha.firebasestorage.app",
+    messagingSenderId: "370851875474",
+    appId: "1:370851875474:web:29b1ba3a76b0fed7d9344b"
   };
 
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
 </script>
